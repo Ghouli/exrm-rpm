86e57f6a8d3cbcc0eb3218c9ab0b06ad4baf858d
pasikok <pasi.kokko@notta.fi>
2018-02-01 09:00:26 +0200

    normalize version containing "+build"

---
+++ test/normalize_version_test.exs
@@ -0,0 +1,22 @@
+defmodule NormalizeVersionTest do
+  use ExUnit.Case
+  alias ReleaseManager.Config
+  alias ReleaseManager.Plugin.Rpm
+  
+  import ReleaseManager.Plugin.Rpm.NormalizeVersion
+
+  test "normalize_version acts correctly" do
+    versions = %{
+        "1.2.3" => "1.2.3",
+        "1.0.0-alpha1" => "0.99.99.99.1",
+        "1.1.0-alpha1" => "1.0.99.99.1",
+        "1.1.1-alpha1" => "1.1.0.99.1",
+        "1.2.3+60" => "1.2.3",
+        "1.2.3-alpha1+60" => "1.2.2.99.1"
+      }
+
+    for {version, expected} <- versions do
+      assert expected == normalize_version(version)
+    end
+  end
+end
